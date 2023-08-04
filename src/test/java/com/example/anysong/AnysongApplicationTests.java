ef90160ee11fcd2b196c854691faed228ddea21d
asugurali <asugurali@greet-go.com>
2023-08-04 17:52:33 +0600

    initial commit

---
+++ src/test/java/com/example/anysong/AnysongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anysong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnysongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
